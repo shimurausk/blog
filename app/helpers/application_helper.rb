0fd5c2b66c06e60e5ada59577ffac4e1555ad8a0
shimurausk <shimurausk@gmail.com>
2015-05-03 11:36:17 +0900

    Change db postgres

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 		max = tags.sort_by(&:count).last
 
 		tags.each do |tag|
-			index = tag.count.to_f / max.count * (classes.size-1)
+			index = tag.count.to_f / Integer(max.count) * (classes.size-1)
 
 			yield(tag,classes[index.round])
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150104154420) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "text"
@@ -31,7 +34,7 @@ ActiveRecord::Schema.define(version: 20150104154420) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
   create_table "dashborads", force: true do |t|
     t.string   "user"
@@ -47,8 +50,8 @@ ActiveRecord::Schema.define(version: 20150104154420) do
     t.datetime "updated_at"
   end
 
-  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id"
-  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
 
   create_table "tags", force: true do |t|
     t.string   "name"
@@ -71,7 +74,7 @@ ActiveRecord::Schema.define(version: 20150104154420) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
