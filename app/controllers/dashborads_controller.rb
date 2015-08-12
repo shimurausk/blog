73f302b06e4cebabfc85e8cab417be1f61be9038
shimurausk <shimurausk@gmail.com>
2015-08-12 14:05:06 +0900

    Change show dashborad order

--- app/controllers/dashborads_controller.rb
+++ app/controllers/dashborads_controller.rb
@@ -1,12 +1,10 @@
 class DashboradsController < ApplicationController
 	before_action :authenticate_user!
 	def index
-		@articles = Article.all
-		# @article = Article.find(params[:article_id])
+		@articles = Article.all.sort().reverse()
 	end
 
 	def show
-		@articles = Article.all
-		# @article = Article.find(params[:article_id])
+		@articles = Article.all.sort().reverse()
 	end
 end
